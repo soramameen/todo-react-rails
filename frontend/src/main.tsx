48beaa0fb62cb28f9113bf9dcf4d8b21aa05285d
中嶋空偉 <nakajimasoraera@MacBookingcom.local>
2025-05-31 13:00:56 +0900

    set-up Apollo Client
    これでfrontendはセットアップ完了

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
   </StrictMode>,
 )
